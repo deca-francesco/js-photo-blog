e885c4f55c85ae4d1bc883aa5d043d5d98eb4dc0
deca-francesco <francescodecarlo70@gmail.com>
2024-10-21 10:43:27 +0200

    ADD: #overlay div closing button function

--- README2.md
+++ README2.md
@@ -5,7 +5,7 @@ Facciamo in modo di creare un overlay che copra l’intera pagina e all’intern
 
 ### Milestone 2
 Facciamo sparire l’overlay con l’aiuto di una classe CSS che imposti il display: none .
-Dopodichè facciamo sì che cliccando una qualunque foto. L’overlay ricompaia.
+Dopodichè facciamo sì che cliccando una qualunque foto l’overlay ricompaia.
 Cliccando invece il button di chiusura, l’overlay scompare nuovamente.
 
 ### Milestone 3
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,15 @@
 console.log("js linked");
 
+// preparo le variabili
 const rowEl = document.querySelector(".container > .row");
 const myCardEl = document.querySelector(".my_card");
 const myCardTopEl = document.querySelector(".my_card_top");
 const myCardBottomEl = document.querySelector(".my_card_bottom");
+const overlayEl = document.getElementById("overlay");
+const overlayimgEl = document.querySelector("#overlay > img");
+const overlayBtnEl = document.querySelector("#overlay > button");
 
-
+// usiamo axios e facciamo una chiamata all’endpoint di JSON Placeholder
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
     .then(response => {
         const albums = response.data;
@@ -13,15 +17,17 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
 
         let albumsMarkup = "";
 
+        // recupero le proprietà e i valori che mi servono da ogni oggetto con la destrutturazione
         albums.forEach(album => {
             const { id, title, thumbnailUrl, url } = album;
 
+            // uso il template literal per inserire i valori
             const markup = `
             <div class="col">
                     <div class="my_card bg-light p-3 d-flex flex-column mx-auto">
                         <div class="my_card_top">
                             <img id="pin" src="./assets/img/pin.svg" alt="pin">
-                            <img src="${url}" alt="random picture">
+                            <a href="#"><img src="${url}" alt="random picture"></a>
                         </div>
                         <div class="my_card_bottom pt-1">
                             <p>${title}</p>
@@ -33,6 +39,14 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
         });
 
         rowEl.innerHTML = albumsMarkup;
-        
+
     })
     .catch(err => console.error(err));
+
+// chiusura overlay
+overlayEl.addEventListener("click", () => {
+    // overlayEl.classList.add("d-none");       // chiude anche se clicco sull'immagine
+})
+overlayBtnEl.addEventListener("click", () => {
+    overlayEl.classList.add("d-none");
+})
