3d1a17e210475d9e0fc2dc477fef1163a2f0e790
deca-francesco <francescodecarlo70@gmail.com>
2024-10-21 11:37:00 +0200

    ADD: #overlay div closing function outside image

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,12 @@ console.log("js linked");
 const rowEl = document.querySelector(".container > .row");
 const myCardEl = document.querySelector(".my_card");
 const myCardTopEl = document.querySelector(".my_card_top");
+const myCardImgEl = document.getElementById("picture");
 const myCardBottomEl = document.querySelector(".my_card_bottom");
 const overlayEl = document.getElementById("overlay");
 const overlayimgEl = document.querySelector("#overlay > img");
 const overlayBtnEl = document.querySelector("#overlay > button");
+const overlayDivEl = document.getElementById("closing_div")
 
 // usiamo axios e facciamo una chiamata all’endpoint di JSON Placeholder
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
@@ -27,7 +29,7 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
                     <div class="my_card bg-light p-3 d-flex flex-column mx-auto">
                         <div class="my_card_top">
                             <img id="pin" src="./assets/img/pin.svg" alt="pin">
-                            <a href="#"><img src="${url}" alt="random picture"></a>
+                            <img id="picture" src="${url}" alt="random picture">
                         </div>
                         <div class="my_card_bottom pt-1">
                             <p>${title}</p>
@@ -40,13 +42,24 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
 
         rowEl.innerHTML = albumsMarkup;
 
+        // 
+        
     })
     .catch(err => console.error(err));
 
-// chiusura overlay
-overlayEl.addEventListener("click", () => {
-    // overlayEl.classList.add("d-none");       // chiude anche se clicco sull'immagine
+
+// apertura overlay
+/*
+myCardImgEl.addEventListener("click", () => {
+    overlayEl.classList.remove("d-none");
 })
+*/
+
+// chiusura overlay
+overlayDivEl.addEventListener("click", () => {
+    overlayEl.classList.add("d-none");
+})
+
 overlayBtnEl.addEventListener("click", () => {
     overlayEl.classList.add("d-none");
 })
